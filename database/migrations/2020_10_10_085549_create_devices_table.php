17287044e38aa31b31c62f37d124afe0aa6f56f4
Nikivas <nikivas97@mail.ru>
2020-10-10 12:12:43 +0300

    models

---
+++ database/migrations/2020_10_10_085549_create_devices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->timestamps();
+            $table->string('mac')->nullable();
+            $table->string('type')->nullable();
+            $table->string('name')->nullable();
+            $table->string('OS')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
